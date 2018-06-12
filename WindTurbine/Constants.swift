712e316e0282bd85c8ae30e49eac2e5271e3f757
mazu20 <maciek789@gmail.com>
2018-06-12 12:17:29 +0200

    test

--- WindTurbine/Constants.swift
+++ WindTurbine/Constants.swift
@@ -6,20 +6,4 @@
 //  Copyright © 2018 Maciej Kowalski. All rights reserved.
 //
 
-import UIKit
-
-struct UserDefaultsKeys {
-    static let APP_OPENED_COUNT = "APP_OPENED_COUNT"
-    static let LEVEL_UP_COUNT = "LEVEL_UP_COUNT"
-}
-
-struct GameCenterIdentifiers {
-    static let POWER_TOTAL_MONEY = "PowerTotalMoney"
-}
-
-struct AdmobAPIKeys {
-    static let APPLICATION_ID = "a"
-    static let REWARD_AD_ID = "b"
-    
-}
 
